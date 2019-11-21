05fe2a02648048d73f96b19f9f5116e3fb749c22
maherlaaroussi <maher.laaroussi@gmail.com>
2019-11-21 11:22:15 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// TODO: Analyse news and categorie
+
+const categories = [
+    'Sciences',
+    'Monde',
+    'Divertisements',
+    'Films',
+    'Séries',
+    'Jeux Vidéos',
+    'Automobile',
+    'Espace',
+    'Technologies',
+    'Politique',
+    'Social',
+    'Juridique'
+]
+
+module.exports = (news) => {
+    if (!news.title && !news.body && !news.category) {
+        if (categories.includes(news.category)) {
+            return true
+        }
+    }
+    return false
+}
